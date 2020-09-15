64fef256cf232c0ca1d69da8c72628a0f10b56dc
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-09-15 21:50:44 +0300

    Finished to fix code after review

--- scripts/repair.js
+++ scripts/repair.js
@@ -2,9 +2,19 @@
 
 const head = document.querySelector('head'),
     cardsBlock = document.querySelector('.repair-cards'),
-    showBtn = document.querySelector('#repairShowBtn');
+    showBtn = document.querySelector('#repairShowBtn'),
+    allLinks = cardsBlock.querySelectorAll('a');
 
-if (document.documentElement.clientWidth < 560) {
+//variables for animation the card-block
+const elemWidth = 260,
+    elemHeight = 90,
+    parentHeight = 150;
+
+//flag for animation card-block
+//used for showing state
+let flag = true;
+
+if (document.documentElement.clientWidth < 563) {
     //create link to slider style-shield
     let styleLink = document.createElement('link');
     styleLink.setAttribute('rel', 'stylesheet');
@@ -25,13 +35,36 @@ if (document.documentElement.clientWidth < 560) {
 }
 
 showBtn.addEventListener('click', () => {
-    cardsBlock.classList.toggle('repair-cards--show-all');
+    let screenWidth = document.documentElement.clientWidth;
+    let elemsAmount = allLinks.length;
+
+    let elemsInRow = 0;
+    
+    //calculate amount elements in the row relatively to screen width
+    if(screenWidth > 1120){
+        elemsInRow = 4;
+    }else{
+        elemsInRow = Math.floor(((screenWidth- 44) / elemWidth));
+    }
+
+    //insert inline style into cards-block element
+    cardsBlock.setAttribute('style', ` height: ${(elemsAmount / elemsInRow) * elemHeight}px;
+                                    transition: height;
+                                    transition-duration: 0.7s;
+                                    transition-timing-function: ease-in;
+    `);
+
     showBtn.classList.toggle('services__readmore-button--active');
 
-    if (cardsBlock.classList.contains('repair-cards--show-all')) {
+    if (flag) {
         showBtn.textContent = 'Скрыть';
+
+        flag = false;
     }else{
         showBtn.textContent = 'Показать все';
+        cardsBlock.removeAttribute('style');
+
+        flag = true;
     }
 
 });
